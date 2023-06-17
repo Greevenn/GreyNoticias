ba6399e648862aed8bf073038b34eff0b3d8ed3a
Greevenn <stevengreyvin00@gmail.com>
2023-06-16 21:47:46 -0600

    Se agrega el Stack con el componente App

--- src/App.js
+++ src/App.js
@@ -3,3 +3,28 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import NewsList from "./NewsList";
 import NewsDetail from "./NewsDetail";
+
+const Stack =createNativeStackNavigator();
+
+function App (){
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                
+                <Stack.Screen
+                name="NewsList"
+                component={NewsList}
+                options={{title: 'Ultimas noticas'}}
+                />
+                 <Stack.Screen
+                name="NewsDetail"
+                component={NewsDetail}
+                options={{title: 'Detalles de Noticas'}}
+                />
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default App;
