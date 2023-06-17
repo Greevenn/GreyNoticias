d3f84c83466a3199eda359a20a175fc05c3c4c51
Greevenn <stevengreyvin00@gmail.com>
2023-06-16 21:52:22 -0600

    Este componente mostrará los detalles de una noticia específica:

--- src/NewsDetail.js
+++ src/NewsDetail.js
@@ -1,3 +1,14 @@
 import React from "react";
 import {View, Text, StyleSheet} from 'react-native'
 import {useRoute} from '@react-navigation/native'
+
+const NewsDetail = ({ navigation}) =>{
+    const route =useRoute();
+    const newsItem = route.params?.newsItem;
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}> {newsItem.title}</Text>
+            <Text style={styles.descripcion}>{newsItem.descripcion}</Text>
+        </View>
+    );
+};
