233be470194c05441e80a8bbc783f68f109bc43d
Greevenn <stevengreyvin00@gmail.com>
2023-06-16 21:44:48 -0600

    Se importaron los modulos necesario al archivo App.js

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import NewsList from "./NewsList";
+import NewsDetail from "./NewsDetail";
